8a818fac91cb8ab9f3b5f985c5dfffc463cca48a
William Harris <17HarriW@fulford.york.sch.uk>
2022-10-12 12:10:28 +0100

    Add project files.

---
+++ PicoBoardController/PicoBoard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoBoardController
+{
+    class PicoBoard
+    {
+        // Source: https://twiki.cern.ch/twiki/pub/Sandbox/DaqSchoolExercise14/Picoboard_protocol.pdf
+        public enum Sensor
+        {
+            RESISTANCE_D = 0,
+            RESISTANCE_C = 1,
+            RESISTANCE_B = 2,
+            BUTTON = 3,
+            RESISTANCE_A = 4,
+            LIGHT = 5,
+            SOUND = 6,
+            SLIDER = 7
+        }
+
+        public int[] sensorValues = new int[8] { 1,2,3,4,5,6,7,8};
+
+        public void Connect()
+        {
+
+        }
+
+        public void Disconnect()
+        {
+
+        }
+
+        public int ReadSensor(int SensorNumber)
+        {
+            return sensorValues[(int)SensorNumber];
+        }
+
+        public bool IsConnected;
+    }
+}
---
+++ PicoBoardController/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoBoardController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PicoBoard p = new PicoBoard();
+            p.Connect();
+
+            int button = p.ReadSensor((int)PicoBoard.Sensor.BUTTON);
+
+            bool running = true;
+            while(running)
+            {
+                Console.WriteLine($"Button: {button}");
+                if(Console.ReadLine() == "quit")
+                {
+                    running = false;
+                }
+            }
+
+
+            p.Disconnect();
+        }
+    }
+}
